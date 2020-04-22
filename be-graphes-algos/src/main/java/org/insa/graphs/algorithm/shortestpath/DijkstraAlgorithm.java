a6c54b1858370ce319845f3b292f9e9d2a03181d
Ben <cadot@etud.insa-toulouse.fr>
2020-04-22 18:58:01 +0200

    Séance 4. Dijkstra presque implémenté.

--- be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/DijkstraAlgorithm.java
+++ be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/DijkstraAlgorithm.java
@@ -1,5 +1,16 @@
 package org.insa.graphs.algorithm.shortestpath;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
+import org.insa.graphs.model.Arc;
+import org.insa.graphs.model.Graph;
+import org.insa.graphs.model.Node;
+import org.insa.graphs.model.Path;
+import org.insa.graphs.model.Label;
+import org.insa.graphs.algorithm.AbstractSolution.Status;
+import org.insa.graphs.algorithm.utils.BinaryHeap;
+
 public class DijkstraAlgorithm extends ShortestPathAlgorithm {
 
     public DijkstraAlgorithm(ShortestPathData data) {
@@ -8,9 +19,76 @@ public class DijkstraAlgorithm extends ShortestPathAlgorithm {
 
     @Override
     protected ShortestPathSolution doRun() {
-        final ShortestPathData data = getInputData();
+
+        // Retrieve the graph.
+        ShortestPathData data = getInputData();
+        Graph graph = data.getGraph();
+
+        final int nbNodes = graph.size();
+        
+        //Array of "Label" 
+        Label[] labels = new Label[nbNodes];
+        for (Node node : graph.getNodes()) {
+        	if (node.equals(data.getOrigin()))
+        		labels[node.getId()] = new Label(node, false, 0, null);
+        	else
+        		labels[node.getId()] = new Label(node, false, Double.POSITIVE_INFINITY, null);
+        }
+        
+        //Heap
+        BinaryHeap<Label> heap = new BinaryHeap<Label>();
+        heap.insert(labels[data.getOrigin().getId()]);
+        
+        // Notify observers about the first event (origin processed).
+        notifyOriginProcessed(data.getOrigin());
+        
+        while (!heap.isEmpty()) {
+        	Node x = heap.deleteMin().getNode();
+        	labels[x.getId()].setTag(true);
+        	for (Arc y : x.getSuccessors()) {
+        		if (!labels[y.getDestination().getId()].getTag()) {
+        			double oldcost = labels[y.getDestination().getId()].getCost();
+        			double newcost = labels[x.getId()].getCost() + y.getLength();
+        			if (oldcost > newcost) {
+        				labels[y.getDestination().getId()].setCost(newcost);
+        				try {
+        					heap.remove(labels[y.getDestination().getId()]);
+        					heap.insert(labels[y.getDestination().getId()]);
+        				}
+        				catch(Exception e) {
+        					heap.insert(labels[y.getDestination().getId()]);
+        				}
+        				finally {
+        					labels[y.getDestination().getId()].setFather(y);
+        				}
+        			}
+        		}
+        	}
+        }
+        
         ShortestPathSolution solution = null;
-        // TODO:
+        if (labels[data.getDestination().getId()].getFather() == null) {
+            solution = new ShortestPathSolution(data, Status.INFEASIBLE);
+        }
+        else {
+
+            // The destination has been found, notify the observers.
+            notifyDestinationReached(data.getDestination());
+            
+        	ArrayList<Arc> arcs = new ArrayList<>();
+        	Arc arc = labels[data.getDestination().getId()].getFather();
+        	while (arc != null) {
+        		arcs.add(arc);
+        		arc = labels[arc.getOrigin().getId()].getFather();
+        	}
+                
+        	// Reverse the path...
+        	Collections.reverse(arcs);
+        	
+        	// Create the final solution.
+        	solution = new ShortestPathSolution(data, Status.OPTIMAL, new Path(graph, arcs));
+        }
+        
         return solution;
     }
 
--- be-graphes-model/src/main/java/org/insa/graphs/model/Label.java
+++ be-graphes-model/src/main/java/org/insa/graphs/model/Label.java
@@ -1,14 +1,15 @@
 package org.insa.graphs.model;
 
-public class Label {
+public class Label implements Comparable<Label>{
 	//attributes
-	public Node currentNode;
-	public boolean tag;
-	public float cost;
-	public Arc father;
+	private Node currentNode;
+	//a tagged Node means it has been visited
+	private boolean tag;
+	private double cost;
+	private Arc father;
 	
 	//constructor
-	Label(Node node, boolean Tag, float Cost, Arc Father) {
+	public Label(Node node, boolean Tag, double Cost, Arc Father) {
 		this.currentNode = node;
 		tag = Tag;
 		cost = Cost;
@@ -16,7 +17,30 @@ public class Label {
 	}
 	
 	//functions
-	float getCost() {
+	public Node getNode() {
+		return this.currentNode;
+	}
+	public double getCost() {
 		return this.cost;
 	}
+	public boolean getTag() {
+		return this.tag;
+	}
+	public Arc getFather() {
+		return this.father;
+	}
+	public void setCost(double Cost) {
+		this.cost = Cost;
+	}
+	public void setTag(boolean Tag) {
+		this.tag = Tag;
+	}
+	public void setFather(Arc Father) {
+		this.father = Father;
+	}
+	
+	@Override
+    public int compareTo(Label other) {
+        return Double.compare(getCost(), other.getCost());
+    }
 }
